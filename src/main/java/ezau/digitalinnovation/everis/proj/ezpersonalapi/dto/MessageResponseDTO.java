9fc229d35e312ca51e49a6c44b910ab5f2003bd8
Ezau Martins <ezaumateus97@gmail.com>
2021-08-19 22:47:49 -0400

    DTOcontrollerMessage

---
+++ src/main/java/ezau/digitalinnovation/everis/proj/ezpersonalapi/dto/MessageResponseDTO.java
@@ -0,0 +1,12 @@
+package ezau.digitalinnovation.everis.proj.ezpersonalapi.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+
+public class MessageResponseDTO {
+
+    private  String message;
+}
