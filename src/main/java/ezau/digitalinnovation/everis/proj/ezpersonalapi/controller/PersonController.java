780c4b28cdb62054e21b0a18a57c2b4d8c19f69c
Ezau Martins <ezaumateus97@gmail.com>
2021-08-20 21:17:01 -0400

    Refat.ControllerServicePerson

--- src/main/java/ezau/digitalinnovation/everis/proj/ezpersonalapi/controller/PersonController.java
+++ src/main/java/ezau/digitalinnovation/everis/proj/ezpersonalapi/controller/PersonController.java
@@ -2,29 +2,26 @@ package ezau.digitalinnovation.everis.proj.ezpersonalapi.controller;
 
 import ezau.digitalinnovation.everis.proj.ezpersonalapi.dto.MessageResponseDTO;
 import ezau.digitalinnovation.everis.proj.ezpersonalapi.entity.Person;
-import ezau.digitalinnovation.everis.proj.ezpersonalapi.repository.PersonRepository;
+import ezau.digitalinnovation.everis.proj.ezpersonalapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController                         //IndicaControladorAcessadoAtravesDeAPIrest
 @RequestMapping("/ApiEz/01/people")     //IndicaLocalDaRequisição(GET)
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
-    @Autowired //Injetar Implemetação repository
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){ //RequestBody Indica que virá de uma requisição
-        // MessageResponseDTO package DTO
-        Person savePerson = personRepository.save(person);        //salva inf.Passada em Person no Repository/Banco
-        return MessageResponseDTO
-                .builder()
-                .message("Created Person with ID " + savePerson.getId())
-                .build();
+        return personService.createPerson(person);
     }
 
 }
